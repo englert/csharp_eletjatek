cb8ddb8414a0e22323aa320a2de1a700d7c38686
Englert Ervin  Brunó <englert@users.noreply.github.com>
2021-12-22 17:06:14 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,94 @@
+// 2021. október 22. 8:00 INFORMATIKAI ISMERETEK EMELT SZINTŰ GYAKORLATI VIZSGA
+// 2. feladat: Életjáték szimulátor1
+using System;
+
+class EletjatekSzimulator
+{
+    private int[ , ] Matrix;    
+    private int      OszlopokSzama;   
+    private int      SorokSzama;      
+   
+    public EletjatekSzimulator(int s, int o)
+    {
+        this.OszlopokSzama = o + 2;
+        this.SorokSzama    = s + 2;
+        this.Matrix = new int[SorokSzama, OszlopokSzama];
+        
+        var rnd = new Random();
+        for(int sor=1; sor < SorokSzama-1; sor++)
+        {
+            for(int oszlop=1; oszlop < OszlopokSzama-1; oszlop++)
+            {  
+                Matrix[sor, oszlop] = rnd.Next(0, 2);
+            }
+        }
+    } //--------- end of method EletjatekSzimulator ---------------------
+
+    private void Megjelenit() 
+    {   
+        for(int sor=0; sor<SorokSzama; sor++)
+        {  
+            for(int oszlop=0; oszlop<OszlopokSzama; oszlop++)
+            {
+                Console.SetCursorPosition(sor, oszlop);
+                if(sor==0 || sor==SorokSzama-1 || oszlop==0 || oszlop == OszlopokSzama-1)
+                {
+                    Console.Write("X");
+                }
+                else
+                {
+                    if (Matrix[sor, oszlop] == 0)  Console.Write(" ");
+                    else                   Console.Write("S");
+                }
+            }
+        }
+    } //--------- end of method Megjelenit ---------------
+
+    private int uj_cella_ertek(int sor, int oszlop)
+    {   
+        var sz =  Matrix[sor-1, oszlop-1] + Matrix[sor-1, oszlop+0] + Matrix[sor-1, oszlop+1]  
+                + Matrix[sor+0, oszlop-1]                           + Matrix[sor+0, oszlop+1] 
+                + Matrix[sor+1, oszlop-1] + Matrix[sor+1, oszlop+0] + Matrix[sor+1, oszlop+1];
+        
+        var letezik     = Matrix[sor, oszlop] == 1;
+        var tuleli      = letezik && (sz == 2 || sz == 3);
+        var uj_szuletik = !letezik && sz == 3;
+        return tuleli || uj_szuletik ? 1 : 0;
+    } //--------- end of method uj_cella_ertek ---------------------
+    
+    private void KovetkezoAllapot()
+    {
+        var ujmatrix = new int[SorokSzama, OszlopokSzama];
+        for( int sor=1; sor < SorokSzama-1; sor++ )
+        {
+            for( int oszlop=1; oszlop < OszlopokSzama-1; oszlop++)
+            {
+                ujmatrix[sor, oszlop] = uj_cella_ertek(sor, oszlop);
+            }
+        }
+        Array.Copy(ujmatrix, Matrix, ujmatrix.Length);
+       
+    } //--------- end of method KovetkezoAllapot ---------------------
+    
+    public void Run() 
+    {
+        //Console.Clear();
+        Megjelenit();
+        KovetkezoAllapot();
+        System.Threading.Thread.Sleep(500);
+    } //--------- end of method Run ---------------------
+
+} // *** end of class EletjatekSzimulator ***
+
+class Program 
+{
+    public static void Main (string[] args) 
+    {
+        var m = new EletjatekSzimulator(20, 20);
+        Console.Clear();
+        while(!Console.KeyAvailable)
+        {
+            m.Run();
+        }
+    } //--------- end of method Main ---------------------
+} // *** end of class Program ***
